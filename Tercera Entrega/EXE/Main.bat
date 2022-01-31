1802d420574e4788cf90e338815399de1a874cf5
JoelCardona01 <98324428+JoelCardona01@users.noreply.github.com>
2022-01-31 09:52:31 +0100

    Add files via upload

---
+++ Tercera Entrega/EXE/Main.bat
@@ -0,0 +1,3 @@
+cd CLASS
+java -cp .;../../FONTS/lib/json-simple-1.1.1.jar --module-path ../../FONTS/lib/javafx-sdk-17.0.1/lib --add-modules javafx.controls,javafx.fxml Presentacio.Main
+cd ..
---
+++ Tercera Entrega/EXE/Main.sh
@@ -0,0 +1,2 @@
+cd CLASS;
+java -cp .:../../FONTS/lib/json-simple-1.1.1.jar --module-path ../../FONTS/lib/javafx-sdk-17.0.1_Ubuntu/lib --add-modules javafx.controls,javafx.fxml Presentacio.Main
